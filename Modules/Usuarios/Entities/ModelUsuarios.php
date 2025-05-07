19f3147eba1a4b91f9c6f0795db6b1bb1aec0806
rafael-p-s <rafaelsky321@gmail.com>
2025-05-07 13:01:51 -0300

    Config user's model but need to complet

---
+++ Modules/Usuarios/Entities/ModelUsuarios.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Modules\Usuarios\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Modules\Usuarios\Database\Factories\ModelUsuariosFactory;
+
+class ModelUsuarios extends Model
+{
+    use HasFactory;
+
+    protected $connetion = 'pgsql';
+    protected $table = 'usuario.usuarios';
+    protected $fillable = [
+        'nome',
+        'sobrenome',
+        'cpf',
+        'telefone',
+        'celular',
+        'endereco',
+        'cidade',
+        'estado',
+        'funcionario',
+        'email',
+        'password',
+        'tipo_cargo',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    public function cargo()
+    {
+        return $this->belongsTo(ModelCargo::class, 'cargo_id');
+    }
+
+    
+
+}
